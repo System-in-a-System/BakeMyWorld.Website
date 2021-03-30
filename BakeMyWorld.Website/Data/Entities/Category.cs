73f8e2a1a0852e122df183a06c5806510fd9b299
Maryna <textconstruction@protonmail.com>
2021-03-30 21:49:32 +0200

    Create Category Entity

---
+++ BakeMyWorld.Website/Data/Entities/Category.cs
@@ -0,0 +1,33 @@
+﻿using Highscores.Website.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.Website.Data.Entities
+{
+    public class Category
+    {
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            UrlSlug = name.Slugify();
+        }
+
+        public int Id { get; protected set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; protected set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UrlSlug { get; protected set; }
+
+        public ICollection<Cake> Cakes { get; protected set; }
+            = new List<Cake>();
+
+    }
+}
