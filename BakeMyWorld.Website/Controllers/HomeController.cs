a7292f397b62efef48e3ca6c1f29a6a2f7bcff6d
Maryna <textconstruction@protonmail.com>
2021-03-30 20:51:36 +0200

    Add OurStory() action method to HomeController

--- BakeMyWorld.Website/Controllers/HomeController.cs
+++ BakeMyWorld.Website/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace BakeMyWorld.Website.Controllers
             return View();
         }
 
+        public IActionResult OurStory()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
