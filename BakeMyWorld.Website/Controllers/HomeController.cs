44bdd7264a751989c592746cfa00da732091847f
Maryna <textconstruction@protonmail.com>
2021-03-30 20:40:38 +0200

    Add AboutUs() action method

--- BakeMyWorld.Website/Controllers/HomeController.cs
+++ BakeMyWorld.Website/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace BakeMyWorld.Website.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
