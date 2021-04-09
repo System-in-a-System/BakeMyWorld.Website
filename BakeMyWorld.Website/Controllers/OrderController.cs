ece46010fb76017c7f752ca1b78ef0ba14101be2
Maryna <textconstruction@protonmail.com>
2021-04-09 11:16:33 +0200

    Create OrderController.cs

---
+++ BakeMyWorld.Website/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using BakeMyWorld.Website.Data;
+using BakeMyWorld.Website.Extensions;
+using BakeMyWorld.Website.Models.Domain;
+using BakeMyWorld.Website.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.Website.Controllers
+{
+    
+    public class OrderController : Controller
+    {
+        private readonly BakeMyWorldContext context;
+
+        public OrderController(BakeMyWorldContext context)
+        {
+            this.context = context;
+        }
+
+        [Route("/checkout")]
+        public IActionResult Checkout()
+        {
+            var viewModel = new CheckOutViewModel
+            {
+                Cart = HttpContext.Session.Get<Cart>("Cart") ?? new Cart()
+            };
+             
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route ("/checkout")]
+        public IActionResult Checkout(CheckOutViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Cart = HttpContext.Session.Get<Cart>("Cart");
+                return View(viewModel);
+            }
+
+            return RedirectToAction(nameof(Confirmation));
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
