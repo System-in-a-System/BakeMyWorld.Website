2c2a201c0ce6651bd71c4a6b2d38a06ee39f049a
arya <arya-battal@hotmail.com>
2021-04-19 10:30:29 +0200

    Add Admin side to Corporate & GiftBox pages

---
+++ BakeMyWorld.Website/Areas/Admin/Models/ViewModels/CreateGiftBoxViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BakeMyWorld.Website.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BakeMyWorld.Website.Areas.Admin.Models.ViewModels
+{
+    public class CreateGiftBoxViewModel
+    {
+   [Required]
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            [Display(Name = "Image URL")]
+            public Uri ImageUrl { get; set; }
+
+            [Required]
+            [Column(TypeName = "decimal(18,2)")]
+            public decimal Price { get; set; }
+
+            public ICollection<Corporate> Corporates { get; set; }
+        }
+}
