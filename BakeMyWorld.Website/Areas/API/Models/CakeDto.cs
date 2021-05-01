db8f21d1c921e026e9a01761420f36790310d9b2
Maryna <textconstruction@protonmail.com>
2021-05-01 21:26:03 +0200

    Create CakeDto.cs

---
+++ BakeMyWorld.Website/Areas/API/Models/CakeDto.cs
@@ -0,0 +1,29 @@
+﻿using BakeMyWorld.Website.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.Website.Areas.API.Models
+{
+    public class CakeDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Uri ImageUrl { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+            = new List<Category>();
+    }
+}
