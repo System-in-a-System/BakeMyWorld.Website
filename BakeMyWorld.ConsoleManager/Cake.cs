a737f67dca5007f23012e96714262041f32674b4
Maryna <textconstruction@protonmail.com>
2021-05-01 21:37:27 +0200

    Create Cake.cs within ConsoleManager

---
+++ BakeMyWorld.ConsoleManager/Cake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.ConsoleManager
+{
+    class Cake
+    {
+        public Cake()
+        {
+
+        }
+
+        public Cake(string name, string description, Uri imageUrl, decimal price)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            Price = price;
+        }
+
+        public Cake(int id, string name, string description, Uri imageUrl, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Uri ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
