a7bf7624bfc95cfd63b834b6d8e34d8288ef994b
Maryna <textconstruction@protonmail.com>
2021-05-01 22:39:13 +0200

    Implement Cakes&Categories Create-Read-Delete functionality from ConsoleManager

--- BakeMyWorld.ConsoleManager/Program.cs
+++ BakeMyWorld.ConsoleManager/Program.cs
@@ -1,12 +1,496 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using static System.Console;
+
 
 namespace BakeMyWorld.ConsoleManager
 {
     class Program
     {
+        static readonly HttpClient httpClient = new HttpClient();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            httpClient.BaseAddress = new Uri("https://localhost:44378/api/");
+
+            CursorVisible = false;
+
+            bool applicationRunning = true;
+
+            do
+            {
+                WriteLine(
+                     @"" +
+                     "\n\t1. Cake Categories" +
+                     "\n\t2. Cakes" +
+                     "\n\t3. Exit");
+
+                ConsoleKeyInfo input = ReadKey(true);
+
+                Clear();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+                        ShowCategoriesMenu();
+                        break;
+
+                    case ConsoleKey.D2:
+                        ShowCakesMenu();
+                        break;
+
+                    case ConsoleKey.D3:
+                        applicationRunning = false;
+                        break;
+                }
+
+            } while (applicationRunning);
+        }
+
+
+
+        private static void ShowCategoriesMenu()
+        {
+            bool applicationRunning = true;
+
+            do
+            {
+                WriteLine(
+                     @"" +
+                     "\n\t1. Register New Category" +
+                     "\n\t2. List Cake Categories" +
+                     "\n\t3. Edit Cake Category" +
+                     "\n\t4. Delete Cake Category" +
+                     "\n\t5. Back");
+
+                ConsoleKeyInfo input = ReadKey(true);
+
+                Clear();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+                        RegisterNewCategoryView();
+                        break;
+
+                    case ConsoleKey.D2:
+                        ListCakeCategories();
+                        EscapeToPreviousMenu();
+                        break;
+
+                    case ConsoleKey.D3:
+                        EditCakeCategory();
+                        break;
+
+                    case ConsoleKey.D4:
+                        DeleteCakeCategory();
+                        break;
+
+                    case ConsoleKey.D5:
+                        applicationRunning = false;
+                        break;
+                }
+
+            } while (applicationRunning);
+        }
+        private static void RegisterNewCategoryView()
+        {
+            // Open prompt loop
+            do
+            {
+                // Prompt
+                SetCursorPosition(2, 2);
+                Write("Category Name: ");
+
+                SetCursorPosition(2, 3);
+                Write("Image Url: ");
+
+
+                // Set cursor to visible
+                CursorVisible = true;
+
+                // Set cursor position to each prompt subsequently & retrieve respecitve information
+                int unifiedIdentation = "Category Name: ".Length + 3;
+
+                SetCursorPosition(unifiedIdentation, 2);
+                string name = ReadLine();
+
+                SetCursorPosition(unifiedIdentation, 3);
+                string imageUrl = ReadLine();
+
+
+                // Further confirmation request
+                ConsoleKeyInfo confirmation = RequestConfirmation();
+
+                // Respond to confirmation choice: "Yes"
+                if (confirmation.Key == ConsoleKey.Y)
+                {
+                    // Instantiate new Category object based on retrieved values
+                    var category = new Category(name, imageUrl);
+
+                    // Set TypeNameHandling to auto
+                    JsonSerializerSettings settings = new JsonSerializerSettings();
+                    settings.TypeNameHandling = TypeNameHandling.Auto;
+
+                    // Serialize to JSON
+                    var httpContent = JsonConvert.SerializeObject(category, settings);
+
+                    // Construct a content object to send the data
+                    var buffer = System.Text.Encoding.UTF8.GetBytes(httpContent);
+                    var byteContent = new ByteArrayContent(buffer);
+
+                    // Set the content type to JSON 
+                    byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                    // Send post request
+                    var response = httpClient.PostAsync("categories", byteContent).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        WriteLine("\n  Category Added");
+                    }
+                    else
+                    {
+                        WriteLine("\n  Something went wrong...");
+                    }
+
+                    Thread.Sleep(2000);
+                    Clear();
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    Clear();
+
+                    // Go back to prompt menu
+                }
+
+            } while (true);
+        }
+        private static void ListCakeCategories()
+        {
+            // HTTP GET https://localhost:44378/api/categories
+            var response = httpClient.GetAsync("categories")
+                .GetAwaiter()
+                .GetResult();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = response.Content.ReadAsStringAsync()
+                    .GetAwaiter()
+                    .GetResult();
+
+                var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(jsonString);
+
+                WriteLine("\n\tId\t\tName");
+                WriteLine("-------------------------------------------------------------");
+
+                foreach (var category in categories)
+                {
+                    WriteLine($"\t{category.Id}\t\t{category.Name}");
+                }
+            }
+            else
+            {
+                WriteLine("\n\tSomething went wrong...");
+            }
+        }
+        private static void EditCakeCategory()
+        {
+            throw new NotImplementedException();
+        }
+        private static void DeleteCakeCategory()
+        {
+            ListCakeCategories();
+
+            Write("\n\tID: ");
+            bool idValid = Int32.TryParse(ReadLine(), out int idParsed);
+            int id = idValid ? idParsed : 0;
+
+            var response = httpClient.DeleteAsync($"categories/{id.ToString()}")
+                .GetAwaiter()
+                .GetResult();
+
+            if (response.IsSuccessStatusCode)
+            {
+                WriteLine("\n\tCategory Deleted");
+            }
+            else
+            {
+                WriteLine("\n\tSomething went wrong...");
+            }
+
+            Thread.Sleep(2000);
+
+            Clear();
+        }
+
+
+        private static void ShowCakesMenu()
+        {
+            bool applicationRunning = true;
+
+            do
+            {
+                WriteLine(
+                     @"" +
+                     "\n\t1. Register New Cake" +
+                     "\n\t2. List Cakes" +
+                     "\n\t3. Edit Cake" +
+                     "\n\t4. Delete Cake" +
+                     "\n\t5. Back");
+
+                ConsoleKeyInfo input = ReadKey(true);
+
+                Clear();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+                        RegisterNewCakeView();
+                        break;
+
+                    case ConsoleKey.D2:
+                        ListCakes();
+                        EscapeToPreviousMenu();
+                        break;
+
+                    case ConsoleKey.D3:
+                        EditCake();
+                        break;
+
+                    case ConsoleKey.D4:
+                        DeleteCake();
+                        break;
+
+                    case ConsoleKey.D5:
+                        applicationRunning = false;
+                        break;
+                }
+
+            } while (applicationRunning);
+        }
+        private static void RegisterNewCakeView()
+        {
+            // Open prompt loop
+            do
+            {
+                // Prompt 
+                SetCursorPosition(2, 2);
+                Write("Name: ");
+
+                SetCursorPosition(2, 3);
+                Write("Description: ");
+
+                SetCursorPosition(2, 4);
+                Write("Image Url: ");
+
+                SetCursorPosition(2, 5);
+                Write("Price: ");
+
+                SetCursorPosition(2, 7);
+                Write("Cake Category: ");
+
+
+                // Set cursor to visible
+                CursorVisible = true;
+
+                // Set cursor position to each prompt subsequently & retrieve respecitve information
+                int unifiedIdentation = "Description: ".Length + 3;
+
+                SetCursorPosition(unifiedIdentation, 2);
+                string name = ReadLine();
+
+                SetCursorPosition(unifiedIdentation, 3);
+                string description = ReadLine();
+
+                SetCursorPosition(unifiedIdentation, 4);
+                string imageUrl = ReadLine();
+
+                SetCursorPosition(unifiedIdentation, 5);
+                bool priceOk = Int32.TryParse(ReadLine(), out int priceParsed);
+                int price = priceOk ? priceParsed : 0;
+
+                SetCursorPosition(unifiedIdentation, 7);
+                string categoryName = ReadLine();
+
+
+                // Further confirmation request
+                ConsoleKeyInfo confirmation = RequestConfirmation();
+
+                // Respond to confirmation choice: "Yes"
+                if (confirmation.Key == ConsoleKey.Y)
+                {
+                    var categoryId = FetchCategoryIdByCategoryName(categoryName);
+
+                    if (categoryId < 0)
+                    {
+                        WriteLine("\n  Indicated Category was not found...");
+                        Thread.Sleep(2000);
+                        Clear();
+                        break;
+                    }
+
+                    // Instantiate new Cake object based on retrieved values
+                    var cake = new Cake(name, description, imageUrl, price, categoryId);
+
+                    // Set TypeNameHandling to auto
+                    JsonSerializerSettings settings = new JsonSerializerSettings();
+                    settings.TypeNameHandling = TypeNameHandling.Auto;
+
+                    // Serialize to JSON
+                    var httpContent = JsonConvert.SerializeObject(cake, settings);
+
+                    // Construct a content object to send the data
+                    var buffer = System.Text.Encoding.UTF8.GetBytes(httpContent);
+                    var byteContent = new ByteArrayContent(buffer);
+
+                    // Set the content type to JSON 
+                    byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                    // Send post request
+                    var response = httpClient.PostAsync("cakes", byteContent).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        WriteLine("\n  Cake Added");
+                    }
+                    else
+                    {
+                        WriteLine("\n  Something went wrong...");
+                    }
+
+                    Thread.Sleep(2000);
+                    Clear();
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    Clear();
+
+                    // Go back to prompt menu
+                }
+
+            } while (true);
+        }
+        private static int FetchCategoryIdByCategoryName(string categoryName)
+        {
+            // HTTP GET https://localhost:44353/api/games
+            var response = httpClient.GetAsync("categories")
+                .GetAwaiter()
+                .GetResult();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = response.Content.ReadAsStringAsync()
+                    .GetAwaiter()
+                    .GetResult();
+
+                var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(jsonString);
+
+                foreach (var category in categories)
+                {
+                    if (category.Name == categoryName)
+                    {
+                        return category.Id;
+                    }
+                }
+            }
+            return -1;
+        }
+        private static void ListCakes()
+        {
+            // HTTP GET https://localhost:44378/api/cakes
+            var response = httpClient.GetAsync("cakes")
+                .GetAwaiter()
+                .GetResult();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = response.Content.ReadAsStringAsync()
+                    .GetAwaiter()
+                    .GetResult();
+
+                var cakes = JsonConvert.DeserializeObject<IEnumerable<Cake>>(jsonString);
+
+                WriteLine("\n\tId\t\tName\t\tPrice");
+                WriteLine("-------------------------------------------------------------");
+
+                foreach (var cake in cakes)
+                {
+                    WriteLine($"\t{cake.Id}\t\t{cake.Name}\t\t{cake.Price}");
+                }
+            }
+            else
+            {
+                WriteLine("\n\tSomething went wrong...");
+            }
+        }
+        private static void EditCake()
+        {
+            throw new NotImplementedException();
+        }
+        private static void DeleteCake()
+        {
+            ListCakes();
+
+            Write("\n\tID: ");
+            bool idValid = Int32.TryParse(ReadLine(), out int idParsed);
+            int id = idValid ? idParsed : 0;
+
+            var response = httpClient.DeleteAsync($"cakes/{id.ToString()}")
+                .GetAwaiter()
+                .GetResult();
+
+            if (response.IsSuccessStatusCode)
+            {
+                WriteLine("\n\tCake Deleted");
+            }
+            else
+            {
+                WriteLine("\n\tSomething went wrong...");
+            }
+
+            Thread.Sleep(2000);
+
+            Clear();
+        }
+
+
+        private static void EscapeToPreviousMenu()
+        {
+            WriteLine("\n\tPress Esc to come back to previous menu");
+
+            ConsoleKeyInfo input = ReadKey(true);
+
+            while (input.Key != ConsoleKey.Escape)
+            {
+                input = ReadKey(true);
+            }
+
+            Clear();
+        }
+        private static ConsoleKeyInfo RequestConfirmation()
+        {
+            WriteLine("\n  Is this correct? (Y)es, (N)o");
+            CursorVisible = false;
+
+            ConsoleKeyInfo confirmation;
+            bool confirmationOk;
+            do
+            {
+                confirmation = ReadKey(true);
+                confirmationOk = confirmation.Key == ConsoleKey.Y || confirmation.Key == ConsoleKey.N;
+
+            } while (!confirmationOk);
+
+            return confirmation;
         }
     }
 }
