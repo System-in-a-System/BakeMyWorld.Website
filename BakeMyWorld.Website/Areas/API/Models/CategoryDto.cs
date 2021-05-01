bdd145e6dfb90dba6ba68f9018cede115b4de435
Maryna <textconstruction@protonmail.com>
2021-05-01 21:26:00 +0200

    Create CategoryDto.cs

---
+++ BakeMyWorld.Website/Areas/API/Models/CategoryDto.cs
@@ -0,0 +1,23 @@
+﻿using BakeMyWorld.Website.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.Website.Areas.API.Models
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public Uri ImageUrl { get; set; }
+
+        public ICollection<Cake> Cakes { get; set; }
+            = new List<Cake>();
+    }
+}
