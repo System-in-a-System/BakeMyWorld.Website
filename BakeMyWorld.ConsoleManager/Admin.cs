4944503c288b53daba7b00c61f98cef4f6ee752f
Maryna <textconstruction@protonmail.com>
2021-05-04 17:43:20 +0200

    Create Admin.cs

---
+++ BakeMyWorld.ConsoleManager/Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.ConsoleManager
+{
+    class Admin
+    {
+        public Admin()
+        {
+
+        }
+        
+        public Admin(string adminName, string adminPassword)
+        {
+            AdminName = adminName;
+            AdminPassword = adminPassword;
+        }
+
+        public string AdminName { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
