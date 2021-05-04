7a6c6092780b89d55739fdea041a751e9b4ea642
Maryna <textconstruction@protonmail.com>
2021-05-04 17:42:06 +0200

    Create AdminDto.cs

---
+++ BakeMyWorld.Website/Areas/API/Models/AdminDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.Website.Areas.API.Models
+{
+    public class AdminDto
+    {
+        public string AdminName { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
