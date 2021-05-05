efe3d68d9d53e3ef42cfa5a4462fecf51cdf6726
Maryna <textconstruction@protonmail.com>
2021-05-05 14:59:17 +0200

    Create TokenDto.cs

---
+++ BakeMyWorld.Website/Areas/API/Models/TokenDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.Website.Areas.API.Models
+{
+    public class TokenDto
+    {
+        public string Value { get; set; }
+    }
+}
