1c30fb5feb8e65f12e2685e9a55fd3cd7ab588d3
Maryna <textconstruction@protonmail.com>
2021-05-05 14:59:12 +0200

    Add Data Annotations to AdminDto

--- BakeMyWorld.Website/Areas/API/Models/AdminDto.cs
+++ BakeMyWorld.Website/Areas/API/Models/AdminDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace BakeMyWorld.Website.Areas.API.Models
 {
     public class AdminDto
     {
+        [Required]
+        [MaxLength(50)]
         public string AdminName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string AdminPassword { get; set; }
     }
 }
