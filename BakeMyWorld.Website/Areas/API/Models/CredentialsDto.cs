55bed7e812cb607c73f0ed822be8c1d858e06829
Maryna <textconstruction@protonmail.com>
2021-05-07 22:45:00 +0200

    Create CredentialsDto.cs

---
+++ BakeMyWorld.Website/Areas/API/Models/CredentialsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.Website.Areas.API.Models
+{
+    public class CredentialsDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Nickname { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+    }
+}
