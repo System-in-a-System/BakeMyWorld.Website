6e4d63632eaa570472874fd99c016c3b7633ef59
Maryna <textconstruction@protonmail.com>
2021-05-07 22:42:29 +0200

    Create User Entity as IdentityUser

---
+++ BakeMyWorld.Website/Data/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakeMyWorld.Website.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+    }
+}
